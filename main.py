e64154d8e19b209d034e99a77d8fb486271cc8b5
Simon Gravelle <simon.gravelle@live.fr>
2023-08-14 19:01:56 +0200

    add function

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import numpy as np
+import sys
 
 n_atoms = 10
-T_init = 1.0
+T = 1.0
 dt = 0.005
 box = np.array([20, 20, 20])
 LJ_cutoff = 3
 delta_dump = 10
+Lx, Ly, Lz = 10, 10, 10
+
+sys.path.append("functions")
+from initialization import initialization
+
+initialization(n_atoms, T, dt, Lx, Ly, Lz)
