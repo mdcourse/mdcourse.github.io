007f096458910d7668a3ba170b1bffc1b73faaf8
Simon Gravelle <simon.gravelle@live.fr>
2023-08-22 17:36:52 +0200

    added gitignore and dump

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import numpy as np
 import logging
+import copy
 import sys
+
+# to remove the runtime warning
+import warnings
+warnings.filterwarnings('ignore')
 
 class MolecularSimulation:
     def __init__(self,
@@ -18,7 +23,9 @@ class MolecularSimulation:
                  displace_mc=0.5,
                  monte_carlo=False,
                  molecular_dynamics=False,
-                 seed=None
+                 seed=None,
+                 thermo=10,
+                 dump=10,
                  ):
         
         self.number_atoms = number_atoms
@@ -35,49 +42,96 @@ class MolecularSimulation:
         self.displace_mc = displace_mc
         self.monte_carlo = monte_carlo
         self.molecular_dynamics = molecular_dynamics
+        self.thermo = thermo
+        self.dump = dump
         if seed is None:
             self.seed = np.random.randint(10000)
         else:
             self.seed = seed
         np.random.seed(self.seed)
 
-        self.kB = 1.38e-23 # J/mol/K
+        self.kB = 1.38e-23 # J/K
+        self.Na = 6.022e23 # mol-1
+        self.J_to_Kcal = 0.0002389
+        self.beta = 1/(self.kB*self.desired_temperature*self.J_to_Kcal*self.Na)  # mol/kCal
+
+    def run(self):
 
         self.initialize_box()
         self.initialize_positions()
+        self.print_log()
+        for step in range(self.maximum_steps):
+            self.step = step
+            if self.monte_carlo:
+                self.monte_carlo_displacement()
+            self.update_log()
+            self.update_dump()
+        self.close_log()
 
-        a = self.calculate_potential_energy()
-        print(a)
+    def update_dump(self, filename="dump.lammpstrj"):
+        if self.step % self.dump == 0:
+            if self.step==0:
+                f = open(filename, "w")
+            else:
+                f = open(filename, "a")
+            f.write("ITEM: TIMESTEP\n")
+            f.write(str(self.step) + "\n")
+            f.write("ITEM: NUMBER OF ATOMS\n")
+            f.write(str(self.number_atoms) + "\n")
+            f.write("ITEM: BOX BOUNDS pp pp pp\n")
+            for dim in np.arange(self.dimensions):
+                f.write(str(self.box_boundaries[dim][0]) + " " + str(self.box_boundaries[dim][1]) + "\n")
+            f.write("ITEM: ATOMS id type x y z\n")
+            for cpt, xyz in enumerate(self.atoms_positions):
+                f.write(str(cpt+1) + " 1 " +str(xyz[0])+" "+str(xyz[1])+" "+str(xyz[2])+"\n") 
+            f.close()
 
     def print_log(self):
         
         # Create and configure logger 
         logging.basicConfig(filename="log.txt", 
-                            format='%(asctime)s %(message)s', 
+                            format='%(message)s', 
                             filemode='w') 
 
         # Create a logger object 
         self.logger=logging.getLogger() 
         self.logger.setLevel(logging.DEBUG)
         self.logger.info("Initialization of the system") 
-        self.logger.info("----------------------------") 
-        self.logger.info("The initial number of atoms is " + str(self.number_atoms)) 
+        self.logger.info("----------------------------\n")
+        self.logger.info("initial number of atoms = " + str(self.number_atoms)) 
         if self.molecular_dynamics:
-            self.logger.info("Initial velocity of CoM = " + str(np.round(self.velocity_com,3).tolist()) + " (NO UNIT SO FAR)") 
-            self.logger.info("Initial kinetic energy = " + str(np.round(self.Ekin,3)) + " (NO UNIT SO FAR)") 
-        self.logger.info("The system is of dimensions " + str(self.dimensions)) 
-        self.logger.info("Box boundaries:") 
+            self.logger.info("initial velocity of CoM = " + str(np.round(self.velocity_com,3).tolist()) + " (NO UNIT SO FAR)") 
+            self.logger.info("initial kinetic energy = " + str(np.round(self.Ekin,3)) + " (NO UNIT SO FAR)") 
+        self.logger.info("initial dimensions = " + str(self.dimensions)) 
+        self.logger.info("\n")
+        self.logger.info("Box boundaries") 
         self.logger.info("xlow = " + str(self.box_boundaries[0][0]) + " Å, xhigh = " + str(self.box_boundaries[0][1]) + " Å") 
         if self.dimensions>1:
             self.logger.info("ylow = " + str(self.box_boundaries[1][0]) + " Å, yhigh = " + str(self.box_boundaries[1][1]) + " Å") 
         if self.dimensions>2:
             self.logger.info("zlow = " + str(self.box_boundaries[2][0]) + " Å, zhigh = " + str(self.box_boundaries[2][1]) + " Å") 
-        self.logger.info("*** Interaction parameters ***") 
+        self.logger.info("\n") 
+        self.logger.info("Interaction parameters") 
         self.logger.info("epsilon = " + str(self.epsilon) + " Kcal/mol") 
         self.logger.info("sigma = " + str(self.sigma) + " Å")
-        self.logger.info("atom mass = " + str(self.atom_mass) + " Å")
-        self.logger.info("*** Monte Carlo parameters ***") 
-        self.logger.info("displace_mc = " + str(self.displace_mc) + " Å")
+        self.logger.info("atom mass = " + str(self.atom_mass) + " Å\n")
+        self.logger.info("Monte Carlo parameters") 
+        self.logger.info("displace_mc = " + str(self.displace_mc) + " Å\n")
+
+    def update_log(self):
+        if self.step == 0:
+            self.logger.info("---------------------------------")
+            self.logger.info("Starting the molecular simulation")
+            self.logger.info("---------------------------------\n")
+            self.logger.info("step Epot")
+        if self.step % self.thermo == 0:
+            self.logger.info(str(self.step) + " " + str(np.round(self.Epot,2)))
+
+    def close_log(self):
+        handlers = self.logger.handlers[:]
+        for handler in handlers:
+            handler.close()
+            self.logger.removeHandler(handler)
 
     def initialize_box(self):
         """Define box boundaries based on Lx, Ly, and Lz.
@@ -135,23 +189,37 @@ class MolecularSimulation:
             kinetic_energy += np.sum(self.atoms_velocities[:, dim]**2)
         self.Ekin = kinetic_energy/self.number_atoms
 
-    def calculate_r(self, position_i):
+    def calculate_r(self, position_i, positions_j):
         """Calculate the shortest distance between position_i and atoms_positions."""
         rij2 = np.zeros(self.number_atoms)
-        rij = (np.remainder(position_i - self.atoms_positions + self.box_size/2., self.box_size) - self.box_size/2.).T
+        rij = (np.remainder(position_i - positions_j + self.box_size/2., self.box_size) - self.box_size/2.).T
         for dim in np.arange(self.dimensions):
             rij2 += np.power(rij[dim, :], 2)
         return np.sqrt(rij2)
 
-    def calculate_potential_energy(self):
+    def calculate_potential_energy(self, atoms_positions):
         """Calculate potential energy assuming Lennard-Jones potential interaction.
         
         Output units are kcal/mol."""
         energy_potential = 0
-        for position_i in self.atoms_positions:
-            r = self.calculate_r(position_i)
+        for position_i in atoms_positions:
+            r = self.calculate_r(position_i, atoms_positions)
             energy_potential_i = np.sum(4*self.epsilon*(np.power(self.sigma/r[r>0], 12)-np.power(self.sigma/r[r>0], 6)))
             energy_potential += energy_potential_i
         # Avoid counting potential energy twice
         energy_potential /= 2
         return energy_potential
+    
+    def monte_carlo_displacement(self):
+        Epot = self.calculate_potential_energy(self.atoms_positions)
+        trial_atoms_positions = copy.deepcopy(self.atoms_positions)
+        atom_id = np.random.randint(self.number_atoms)
+        trial_atoms_positions[atom_id] += (np.random.random(3)-0.5)*self.displace_mc
+        trial_Epot = self.calculate_potential_energy(trial_atoms_positions)
+        acceptation_probability = np.min([1, np.exp(-self.beta*(trial_Epot-Epot))])
+        if np.random.random() <= acceptation_probability:
+            self.atoms_positions = trial_atoms_positions
+            self.Epot = trial_Epot
+        else:
+            self.Epot = Epot
+            
