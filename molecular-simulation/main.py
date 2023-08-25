b25f83a9b25a741ef246dd5d25f9d59881140f1e
Simon Gravelle <simon.gravelle@live.fr>
2023-08-25 23:07:24 +0200

    added MD class

--- molecular-simulation/main.py
+++ molecular-simulation/main.py
@@ -9,6 +9,133 @@ import matplotlib.pyplot as plt
 import warnings
 warnings.filterwarnings('ignore')
 
+class InitializeSimulation:
+    def __init__(self,
+                 number_atoms,
+                 Lx,
+                 dimensions=3,
+                 Ly = None,
+                 Lz = None,
+                 epsilon=0.1,
+                 sigma=1,
+                 atom_mass=1,
+                 seed=None,
+                 desired_temperature=300
+                 ):
+        self.number_atoms = number_atoms
+        self.Lx = Lx
+        self.Ly = Ly
+        self.Lz = Lz
+        self.dimensions = dimensions
+        self.epsilon = epsilon
+        self.sigma = sigma
+        self.atom_mass = atom_mass
+        self.seed = seed
+        self.desired_temperature = desired_temperature
+
+        self.reference_distance = self.sigma
+        self.reference_energy = self.epsilon
+        self.reference_mass = self.atom_mass
+        self.reference_time = np.sqrt((self.reference_mass/cst.kilo/cst.Avogadro)*(self.reference_distance*cst.angstrom)**2/(self.reference_energy*cst.calorie*cst.kilo/cst.Avogadro))/cst.femto
+
+        self.Lx = self.nondimensionalise_units(self.Lx, "distance")
+        self.Ly = self.nondimensionalise_units(self.Ly, "distance")
+        self.Lz = self.nondimensionalise_units(self.Lz, "distance")
+        self.epsilon = self.nondimensionalise_units(self.epsilon, "energy")
+        self.sigma = self.nondimensionalise_units(self.sigma, "distance")
+        self.atom_mass = self.nondimensionalise_units(self.atom_mass, "mass")
+        self.desired_temperature = self.nondimensionalise_units(self.desired_temperature, "temperature")
+
+        self.initialize_box()
+        self.populate_box()
+        self.give_velocity()
+        self.write_lammps_data(filename="initial.data")
+
+    def nondimensionalise_units(self, variable, type):
+        kB = cst.Boltzmann*cst.Avogadro/cst.calorie/cst.kilo # kCal/mol/K
+        if variable is not None:
+            if type == "distance":
+                variable /= self.reference_distance
+            elif type == "energy":
+                variable /= self.reference_energy
+            elif type == "mass":
+                variable /= self.reference_mass
+            elif type == "temperature":
+                variable /= self.reference_energy/kB
+            else:
+                print("Unknown variable type", type)
+        return variable
+
+    def initialize_box(self):
+        """Define box boundaries based on Lx, Ly, and Lz.
+
+        If Ly or Lz or both are None, then Lx is used instead"""
+        box_boundaries = np.zeros((self.dimensions, 2))
+        for dim, L in zip(range(self.dimensions), [self.Lx, self.Ly, self.Lz]):
+            if L is not None:
+                box_boundaries[dim] = -L/2, L/2
+            else:
+                box_boundaries[dim] = -self.Lx/2, self.Lx/2
+        box_boundaries = self.nondimensionalise_units(box_boundaries, "distance")
+        self.box_boundaries = box_boundaries
+
+    def populate_box(self):
+        """Place atoms at random positions within the box."""
+        atoms_positions = np.zeros((self.number_atoms, self.dimensions))
+        for dim in np.arange(self.dimensions):
+            atoms_positions[:, dim] = np.random.random(self.number_atoms)*np.diff(self.box_boundaries[dim]) - np.diff(self.box_boundaries[dim])/2    
+        self.atoms_positions = atoms_positions
+
+    def give_velocity(self):
+        """Give velocity to atoms so that the initial temperature is the desired one."""
+        atoms_velocities = np.zeros((self.number_atoms, self.dimensions))
+        for dim in np.arange(self.dimensions):  
+            atoms_velocities[:, dim] = np.random.normal(size=self.number_atoms)
+        atoms_velocities *= np.sqrt(self.desired_temperature/self.atom_mass/self.dimensions)
+        self.atoms_velocities = atoms_velocities
+
+    def write_lammps_data(self, filename="lammps.data"):
+        """Write a LAMMPS data file containing atoms positions and velocities"""
+        f = open(filename, "w")
+        f.write('# LAMMPS data file \n\n')
+        f.write(str(self.number_atoms)+' atoms\n')
+        f.write('1 atom types\n')
+        f.write('\n')
+        for LminLmax, dim in zip(self.box_boundaries*self.reference_distance, ["x", "y", "z"]):
+            f.write(str(LminLmax[0])+' '+str(LminLmax[1])+' '+dim+'lo ' + dim +  'hi\n')
+        f.write('\n')
+        f.write('Atoms\n')
+        f.write('\n')
+        cpt = 1
+        for xyz in self.atoms_positions*self.reference_distance:
+            f.write(str(cpt)+ ' 1 ' + str(xyz[0]) + ' ' + str(xyz[1]) + ' ' + str(xyz[2]) +'\n')
+            cpt += 1
+        f.write('\n')
+        f.write('Velocities\n')
+        f.write('\n')
+        cpt = 1
+        for vxyz in self.atoms_velocities*self.reference_distance/self.reference_time:
+            f.write(str(cpt) + ' ' + str(vxyz[0]) + ' ' + str(vxyz[1]) + ' ' + str(vxyz[2]) +'\n')
+            cpt += 1
+        f.close()
+
+class MolecularDynamics(InitializeSimulation):
+
+    def _init__(self,
+                maximum_steps,
+                tau_temp = None,
+                *args,
+                **kwargs,
+                ):
+        super().__init__(*args, **kwargs)
+        self.maximum_steps = maximum_steps
+        self.tau_temp = tau_temp  
+
+    def run(self):
+
+        for self.step in range(1, self.maximum_steps+1):
+             print(self.step)
+
 class MolecularSimulation:
     def __init__(self,
                  number_atoms,
