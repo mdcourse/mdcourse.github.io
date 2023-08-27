8142761d293912f20a1710549cfca30f6c353de1
simongravelle <simongravelle@users.noreply.github.com>
2023-08-27 09:25:53 +0000

    deploy: fbde95f368c2bdddceac3edcfbc0a4aec64edbaf

---
+++ source/conf.py
@@ -0,0 +1,17 @@
+# Configuration file for the Sphinx documentation builder.
+
+project = 'learn-ms-with-python'
+copyright = 'All source code is available under the GNU General Public License v3.0'
+author = 'Simon Gravelle'
+release = '0.0.0'
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = "sphinx_book_theme"
+html_title = "    "
+html_static_path = ['_static']
+
+html_logo = "_static/logo/logo-lmdwp.png"
